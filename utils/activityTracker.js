61f51dfe6090612c79d76a5fd267edcdc7078b4d
Key Usaju <keyusaju0@gmail.com>
2026-04-09 15:59:43 +0700

    repair 2

--- utils/activityTracker.js
+++ utils/activityTracker.js
@@ -1,35 +1,21 @@
-const fs = require('fs');
-const path = './activity.json';
+let activity = {};
 
-function load() {
-  if (!fs.existsSync(path)) fs.writeFileSync(path, JSON.stringify({}));
-  return JSON.parse(fs.readFileSync(path));
-}
+function markActive(userId) {
+  if (!activity[userId]) {
+    activity[userId] = 0;
+  }
 
-function save(data) {
-  fs.writeFileSync(path, JSON.stringify(data, null, 2));
-}
-
-function track(userId) {
-  const data = load();
-
-  if (!data[userId]) data[userId] = 0;
-
-  data[userId] += 1;
-
-  save(data);
+  activity[userId]++;
 }
 
 function getTopUser() {
-  const data = load();
-
   let topUser = null;
   let max = 0;
 
-  for (const user in data) {
-    if (data[user] > max) {
-      max = data[user];
-      topUser = user;
+  for (const userId in activity) {
+    if (activity[userId] > max) {
+      max = activity[userId];
+      topUser = userId;
     }
   }
 
@@ -37,7 +23,11 @@ function getTopUser() {
 }
 
 function reset() {
-  save({});
+  activity = {};
 }
 
-module.exports = { track, getTopUser, reset };
+module.exports = {
+  markActive,
+  getTopUser,
+  reset
+};
